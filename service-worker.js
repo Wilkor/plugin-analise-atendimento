791e784fc311d4aeac41adc7a1f7e4b6609daf8b
Wilkor <wilkorguitarrista@msn.com>
2024-03-28 13:47:51 -0300

    melhorias
    Ajustando o filtro quando sai o resultado o analista filtrado

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.121f01dfb0aba1d7f9c869bbbd17af89.js"
+  "./precache-manifest.638593fdd3bcbcc2605b4c87316774c0.js"
 );
 
 self.addEventListener('message', (event) => {
